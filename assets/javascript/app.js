b268051cbe61745d336535b179e665eb5e3b20b8
Dmitrii Zakharov <dzakharov@splunk.com>
2018-09-12 20:38:53 -0700

    Only styling left

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "
         var animal = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=fapyKRiT6KIoUlJqZLaAl5VHUIf5Q3cs&limit=10&q=" + animal;
 
-        // Creating an AJAX call for the specific movie button being clicked
+        // Creating an AJAX call for the specific animal button being clicked
         $.ajax({
           url: queryURL,
           method: "GET"
@@ -15,7 +15,7 @@ var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "
 
           var animalDiv = $("<div class='animal'>");
 
-          // Creating a div to hold the movie
+          // Creating a div to hold the animal
           var animalView = $("#animal-view");
 
           console.log(response);
@@ -40,45 +40,26 @@ var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "
             animalDiv.append(image);
           }
 
-        //   // Storing the plot
-        //   var plot = response.Plot;
-
-        //   // Creating an element to hold the plot
-        //   var pThree = $("<p>").text("Plot: " + plot);
-
-        //   // Appending the plot
-        //   movieDiv.append(pThree);
-
-        //   // Retrieving the URL for the image
-        //   var imgURL = response.Poster;
-
-        //   // Creating an element to hold the image
-        //   var image = $("<img>").attr("src", imgURL);
-
-        //   // Appending the image
-        //   movieDiv.append(image);
-
-        //   // Putting the entire movie above the previous movies
-           $("#animal-view").prepend(animalDiv);
+           $("#animal-view").append(animalDiv);
         });
 
       }
 
-      // Function for displaying movie data
+      // Function for displaying animal data
       function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
+        // Deleting the animals prior to adding new animals
         // (this is necessary otherwise you will have repeat buttons)
         $("#buttons-view").empty();
 
-        // Looping through the array of movies
+        // Looping through the array of animals
         for (var i = 0; i < animals.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
+          // Then dynamicaly generating buttons for each animal in the array
           // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("movie-btn");
+          // Adding a class of animal-btn to our button
+          a.addClass("animal-btn");
           // Adding a data-attribute
           a.attr("data-name", animals[i]);
           // Providing the initial button text
@@ -88,21 +69,25 @@ var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "
         }
       }
 
-      // This function handles events where a movie button is clicked
-      $("#add-movie").on("click", function(event) {
+      // This function handles events where a animal button is clicked
+      //$("#add-animal").on("click", function(event) {
+
+      $(document).on("click", "#add-animal", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
-        var animal = $("#movie-input").val().trim();
+        var animal = $("#animal-input").val().trim();
 
-        // Adding movie from the textbox to our array
+        alert(animal);
+
+        // Adding animal from the textbox to our array
         animals.push(animal);
 
-        // Calling renderButtons which handles the processing of our movie array
+        // Calling renderButtons which handles the processing of our animal array
         renderButtons();
       });
 
-      // Adding a click event listener to all elements with a class of "movie-btn"
-      $(document).on("click", ".movie-btn", displayAnimalGifs);
+      // Adding a click event listener to all elements with a class of "animal-btn"
+      $(document).on("click", ".animal-btn", displayAnimalGifs);
 
 
       
@@ -116,11 +101,6 @@ var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "
       renderButtons();
     });
 
-
-    //$(".gif").on('click', function() {
-    //$(document).on("click", ".gif", clickOnGif(this));
-    
-    //function clickOnGif(){
 
     $(document).on("click", ".gif", function(){
         //alert("Clicked");
